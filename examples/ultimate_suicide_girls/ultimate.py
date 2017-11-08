4b08878007f7cd88af40fa2fbbd4063d36f7a2d3
borjarobles <borja.robles@gmail.com>
2017-11-08 09:27:19 +0100

    Add 2 upload logics

--- examples/ultimate_suicide_girls/ultimate.py
+++ examples/ultimate_suicide_girls/ultimate.py
@@ -17,6 +17,9 @@ from random import shuffle
 from bs4 import BeautifulSoup
 import threading
 import schedule
+from InstagramAPI import InstagramAPI
+
+
 
 sys.path.append(os.path.join(sys.path[0], '../../'))
 from instabot import Bot
@@ -55,7 +58,7 @@ bot.login(username=args.u, password=args.p,
           proxy=args.proxy)
 
 
-bot.logger.info("ULTIMATE RAUL")
+bot.logger.info("ULTIMATE SUICIDE GIRLS")
 comments_file_name = "comments.txt"
 
 
@@ -123,16 +126,32 @@ def job6():
 
 def job7():
     try:
+        user,pwd = 'cortneyrodriguizs', 'insta@123'
+
+        API = InstagramAPI(user,pwd)
+        API.login() # login
+
         random_filename = random.choice([
             x for x in os.listdir('pics')
         ])
 
         photo_path = 'pics/' + random_filename
 
-        print("upload: " + caption)
-        bot.uploadPhoto(photo_path, caption=caption)
-        if bot.LastResponse.status_code != 200:
-            print(bot.LastResponse)
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+        r=requests.get('https://top-hashtags.com/instagram/', headers=headers)
+        soup = BeautifulSoup(r.content, 'html.parser')
+
+        thetags = []
+
+        for tag in soup.find_all("div", {'class': 'tht-tag'}, 'a'):
+            thetags.append(tag.string.encode('ascii','ignore'))
+
+        cut = random.sample(thetags, 24)
+        same = ['#follow4follow', '#f4f', '#TagsForLikes', '#like4like', '#instafollow', '#followme']
+        test = cut.extend(same)
+        caption = " ".join(str(x) for x in cut)
+
+        API.uploadPhoto(photo_path, caption=caption)
     except Exception as e:
         print(str(e))
 
@@ -152,7 +171,6 @@ def job9():
     except Exception as e:
         print(str(e))
 
-
 # function to make threads -> details here http://bit.ly/faq_schedule
 def run_threaded(job_fn):
     job_thread=threading.Thread(target=job_fn)
@@ -160,14 +178,14 @@ def run_threaded(job_fn):
 
 schedule.every(1).hour.do(run_threaded, stats)              # get stats
 schedule.every(6).hours.do(run_threaded, job1)              # like hashtag
-schedule.every(2).hours.do(run_threaded, job2)              # like timeline
-schedule.every(8).hours.do(run_threaded, job3)              # comment timeline medias
-schedule.every(1).days.at("16:00").do(run_threaded, job4)   # Like users medias
+schedule.every(5).hours.do(run_threaded, job2)              # like timeline
+schedule.every(12).hours.do(run_threaded, job3)             # comment timeline medias
+schedule.every(1).days.at("12:00").do(run_threaded, job4)   # Like users medias
 schedule.every(6).hours.do(run_threaded, job5)              # Going to follow followers
 schedule.every(6).hours.do(run_threaded, job6)              # Follow users
-schedule.every(4).hours.do(run_threaded, job7)              # Upload pic
+schedule.every(6).hours.do(run_threaded, job7)              # Upload pic
 schedule.every(1).days.at("10:00").do(run_threaded, job8)   # Unfollow non followers
-schedule.every(5).days.do(run_threaded, job9)               # Renew images
+schedule.every(6).days.do(run_threaded, job9)               # Renew images
 
 
 while True:
